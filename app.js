c1879f6c45b65c7f6f0c16444a1c1c037a1a9f11
adibush <121572850+adibush@users.noreply.github.com>
2026-06-12 14:27:22 +0300

    Fix typo in response message

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('my first adi11');
+  res.end('my first adi111');
 });
 
 server.listen(port, hostname, () => {
