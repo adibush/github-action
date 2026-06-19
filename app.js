44a02ae71d1f2d6c698c193fa6ca240b4acb17e9
adi <adibush01@gmail.com>
2026-06-19 22:47:19 +0300

    SAdsadasdas

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-<<<<<<< HEAD
-  res.end('hello from adi bashari ');
-=======
   res.end('hello test 1000000 ');
->>>>>>> 6074e0bf70d792d92668976531023e4672dec471
 });
 
 server.listen(port, hostname, () => {
